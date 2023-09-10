150783fe8cd9affa59546f4548b7055a9b1cd08c
ilham b <durianbohong@gmail.com>
2023-09-10 13:57:13 +0700

    Changes to be committed: 	modified:   .gitignore 	new file:   .postcssrc 	new file:   dist/Home.09cfde9d.js 	new file:   dist/Home.09cfde9d.js.map 	new file:   dist/il4mb.github.io.59712999.js 	new file:   dist/il4mb.github.io.59712999.js.map 	new file:   dist/index.322235f5.css 	new file:   dist/index.322235f5.css.map 	new file:   dist/index.b7a05eb9.js 	new file:   dist/index.b7a05eb9.js.map 	new file:   dist/index.html 	new file:   package.json 	new file:   src/App.vue 	new file:   src/assets/source.css 	new file:   src/index.html 	new file:   src/main.ts 	new file:   src/shims-vue.d.ts 	new file:   src/views/Home.vue 	new file:   tailwind.config.js 	new file:   yarn.lock

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="bg-gray-800 text-white fixed top-0 w-full z-10">
+        <div class="flex items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-[70px]">
+            <router-link to="/">
+                <span class="text-2xl font-bold text-white">Online Store</span>
+            </router-link>
+
+            <button class="ms-auto text-lg opacity-80 md:hidden w-8 h-8 text-white rounded-sm" @click="triggerMenu">
+                <font-awesome-icon icon="bars" />
+            </button>
+
+            <div class="navbar-menu p-0">
+                <div class="flex justify-end">
+                    <button class="text-xl opacity-80 md:hidden w-8 h-8 text-gray-800 rounded-sm" @click="triggerMenu">
+                        <font-awesome-icon icon="times" />
+                    </button>
+                </div>
+                <router-link to="/" class="mx-3">
+                    <font-awesome-icon icon="store" /> Home
+                </router-link>
+                <router-link to="/cart" class="mx-3">
+                    <font-awesome-icon :icon="['fas', 'cart-shopping']" /> Cart
+                </router-link>
+                <router-link to="/about" class="mx-3">
+                    <font-awesome-icon icon="info-circle" /> About
+                </router-link>
+            </div>
+        </div>
+    </nav>
+    <main class="max-h-screen overflow-hidden">
+        <router-view />
+    </main>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            component: () => import("./views/Home.vue")
+        }
+    ]
+})
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
