fc1e20e2725bc214cd0c6da3173655cb96271d71
ilham b <durianbohong@gmail.com>
2023-09-13 00:27:28 +0700

    Changes to be committed: 	new file:   dist/Amik-Riau.794587ac.png 	new file:   dist/Amik-riau.e6ec4c3a.png 	new file:   dist/Baba-1.76e50a85.jpg 	new file:   dist/Codecademy-1.d9035b87.jpg 	new file:   dist/Dicoding-1.1a992c8c.jpg 	new file:   dist/Dicoding-2.9fc23269.jpg 	new file:   dist/Dicoding-3.6899a02b.jpg 	new file:   dist/Dicoding-4.291c3e6d.jpg 	new file:   dist/IBMI-1.50b50400.jpg 	new file:   dist/OracleA-1.2061ae32.jpg 	new file:   dist/PKKMB.7c558bc6.jpg 	modified:   dist/index.322235f5.css 	modified:   dist/index.322235f5.css.map 	new file:   dist/index.6087dd4d.js 	new file:   dist/index.6087dd4d.js.map 	new file:   dist/index.674414c6.js 	new file:   dist/index.674414c6.js.map 	modified:   dist/index.b7a05eb9.js 	modified:   dist/index.b7a05eb9.js.map 	new file:   dist/index.fba2bfdd.css 	new file:   dist/index.fba2bfdd.css.map 	new file:   dist/sertifikat-codecademy.b5b86301.jpg 	modified:   package-lock.json 	modified:   package.json 	new file:   src/assets/images/Baba-1.jpg 	new file:   src/assets/images/Codecademy-1.jpg 	new file:   src/assets/images/Dicoding-1.jpg 	new file:   src/assets/images/Dicoding-2.jpg 	new file:   src/assets/images/Dicoding-3.jpg 	new file:   src/assets/images/Dicoding-4.jpg 	new file:   src/assets/images/IBMI-1.jpg 	new file:   src/assets/images/Il4mb's profile _ Codecademy.pdf 	new file:   src/assets/images/OracleA-1.jpg 	new file:   src/assets/images/PKKMB.jpg 	new file:   src/assets/images/organization/Amik-Riau.png 	new file:   src/assets/pdf/Il4mb_s profile _ Codecademy.pdf 	new file:   src/assets/pdf/Ilham-BBasics-of-Project-Management-8211-ExamCourse-CertificateInternational-Business-Management-Institute.pdf 	new file:   src/assets/pdf/Record-25_2.pdf 	new file:   src/assets/pdf/clmsCertificate.pdf 	new file:   src/assets/pdf/sertifikat-0.pdf 	new file:   src/assets/pdf/sertifikat_course_120_2624255_190223222841.pdf 	new file:   src/assets/pdf/sertifikat_course_169_2624255_180223162440.pdf 	new file:   src/assets/pdf/sertifikat_course_256_2624255_180223143006.pdf 	new file:   src/assets/pdf/sertifikat_course_261_2624255_180223143747.pdf 	new file:   src/data/certificate.json.js 	modified:   src/router.ts 	modified:   src/views/AboutMe.vue 	modified:   yarn.lock

---
+++ src/data/certificate.json.js
@@ -0,0 +1,114 @@
+import c1 from "../assets/images/Codecademy-1.jpg";
+import c2 from "../assets/images/Dicoding-1.jpg";
+import c3 from "../assets/images/Dicoding-2.jpg";
+import c4 from "../assets/images/Dicoding-3.jpg";
+import c5 from "../assets/images/Dicoding-4.jpg";
+import c6 from "../assets/images/PKKMB.jpg";
+import c7 from "../assets/images/Baba-1.jpg";
+import c8 from "../assets/images/OracleA-1.jpg";
+import c9 from "../assets/images/IBMI-1.jpg";
+
+import o1 from "../assets/images/organization/Amik-Riau.png";
+
+const organization = {
+    "dicoding": {
+        "name": "Dicoding",
+        "logo": "https://avatars.githubusercontent.com/u/22091590?s=280&v=4"
+    },
+    "codecademy": {
+        "name": "Codecademy",
+        "logo": "https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515403_10914.png"
+    },
+    "amik-riau": {
+        "name": "STMIK AMIK RIAU",
+        "logo": o1
+    },
+    "oracle": {
+        "name": "Oracle",
+        "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Oracle_Corporation_logo.svg/1200px-Oracle_Corporation_logo.svg.png"
+    },
+    "baba": {
+        "name": "Baba Studio",
+        "logo": "https://images.glints.com/unsafe/glints-dashboard.s3.amazonaws.com/company-logo/2132096ac3b89902b8c0f488e1a5ee54.png"
+    },
+    "ibmi": {
+        "name": "International Business Management Institute",
+        "logo": "https://www.ibm-institute.com/wp-content/uploads/2017/12/newlogofb.png"
+    }
+}
+
+module.exports = [
+    {
+        "name": "Full-Stack Engineer Carrer Path",
+        "organization": organization.codecademy,
+        "issuingdate": "Sep 2023",
+        "expirationdate": "",
+        "credential": "",
+        "file": c1
+    },
+    {
+        "name": "Basic Project Management",
+        "organization": organization.ibmi,
+        "issuingdate": "Sep 2023",
+        "expirationdate": "",
+        "credential": "",
+        "file": c9
+    },
+    {
+        "name": "Pemrograman Dengan C",
+        "organization": organization.dicoding,
+        "issuingdate": "Feb 2023",
+        "expirationdate": "Feb 2026",
+        "credential": "",
+        "file": c2
+    },
+    {
+        "name": "Belajar Prinsip Pemograman Solid",
+        "organization": organization.dicoding,
+        "issuingdate": "Feb 2023",
+        "expirationdate": "Feb 2026",
+        "credential": "",
+        "file": c3
+    },
+    {
+        "name": "Belajar Dasar Pemograman Dengan JavaScript",
+        "organization": organization.dicoding,
+        "issuingdate": "Feb 2023",
+        "expirationdate": "Feb 2026",
+        "credential": "https://avatars.githubusercontent.com/u/22091590?s=280&v=4",
+        "file": c4
+    },
+    {
+        "name": "Membuat Aplikasi Backend untuk Pemula",
+        "organization": organization.dicoding,
+        "issuingdate": "Feb 2023",
+        "expirationdate": "Feb 2026",
+        "credential": "",
+        "file": c5
+    },
+    {
+        "name": "PKKMB",
+        "organization": organization["amik-riau"],
+        "issuingdate": "Sep 2022",
+        "expirationdate": "",
+        "credential": "",
+        "file": c6
+    },
+    {
+        "name": "Implementasi Modren Website React.js",
+        "organization": organization.baba,
+        "issuingdate": "Sep 2022",
+        "expirationdate": "",
+        "credential": "",
+        "file": c7
+    },
+    {
+        "name": "Java Fundamental",
+        "organization": organization.oracle,
+        "issuingdate": "Jul 2023",
+        "expirationdate": "",
+        "credential": "",
+        "file": c8
+    },
+
+]
